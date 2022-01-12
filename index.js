709d7fc9b82e015d1cd41bb71e0cdee8434b9e76
Rebekah Zhou <rebekahzhou@gmail.com>
2022-01-12 08:51:16 -0800

    commit tree 77dd2a787473e70145f925c1514a302d6a7d496c

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(arrDrivers, name) {
+    const arrMatching = arrDrivers.filter(element => element === name || element === name.toLowerCase());
+    return arrMatching;
+}
+
+function fuzzyMatch(arrDrivers, name) {
+    const arrMatching = arrDrivers.filter(element => element[0] === name[0])
+    return arrMatching;
+}
+
+function matchName(arrDrivers, name) {
+    const arrMatching = arrDrivers.filter(element => element.name === name)
+    return arrMatching;
+}
